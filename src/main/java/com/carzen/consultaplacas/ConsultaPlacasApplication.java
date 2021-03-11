979d0f024e51e12308fac43cc5b05a833c4bf3c2
Lorena Gomes <logcy@yahoo.com.br>
2021-03-10 21:31:05 -0300

    Criando projeto spring

---
+++ src/main/java/com/carzen/consultaplacas/ConsultaPlacasApplication.java
@@ -0,0 +1,13 @@
+package com.carzen.consultaplacas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaPlacasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaPlacasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carzen/consultaplacas/ConsultaPlacasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carzen.consultaplacas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaPlacasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
